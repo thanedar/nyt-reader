473285e1acfc7ae4f9f4c347444d2f7295066c1a
Sudhanshu Thanedar <sudhanshu.thanedar@acotel.com>
2016-05-23 13:46:16 -0400

    added retrofit interface and client class

---
+++ app/src/main/java/com/suswara/newsreader_retro/rest/ApiClient.java
@@ -0,0 +1,22 @@
+package com.suswara.newsreader_retro.rest;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+public class ApiClient {
+
+    public static final String BASE_URL = "https://api.nytimes.com/svc/";
+    private static Retrofit retrofit = null;
+
+
+    public static Retrofit getClient() {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
---
+++ app/src/main/java/com/suswara/newsreader_retro/rest/ApiInterface.java
@@ -0,0 +1,18 @@
+package com.suswara.newsreader_retro.rest;
+
+import com.suswara.newsreader_retro.beans.TopStories;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+
+public interface ApiInterface {
+    @GET("topstories/v2/home.json")
+    Call<TopStories> getTopStories(@Query("api_key") String apiKey);
+
+    /*@GET("movie/{id}")
+    Call<MoviesResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
+    */
+}
